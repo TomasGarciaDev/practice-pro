cf385949687143e396a7577a77f9e06a1bd5a19d
TomasGarciaDev <tomasgarciadev@gmail.com>
2025-03-12 13:23:55 +0100

    Styles: homepage all breakpoints

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,33 @@
 export default function Home() {
   return (
-    <div>
-      <header className='w-full h-20 text-center bg-[#31363F] text-white'>
-        HEADER
+    <div className='relative bg-home-img bg-cover bg-center bg-fixed min-h-screen'>
+      {/* Background Overlay */}
+      <div className='absolute inset-0 bg-[#146C94]/50'></div>
+
+      {/* Header */}
+      <header className='relative w-full h-20 bg-[#146C94] flex items-center'>
+        <h1 className='text-white ml-8 text-4xl'>HEADER</h1>
       </header>
-      <div className='bg-gray-400 bg-home-img bg-cover bg-center bg-fixed'>
-        <main className='flex flex-col w-full h-dvh'>
-          <div className='flex flex-col gap-6 p-12 text-center rounded-xl bg-black/70 w-4/5 mx-auto my-8 text-white'>
-            <h1 className='text-4xl font-bold'>Welcome To Practice Pro</h1>
-            <p className='text-2xl'>Your Best Music Practice Diary</p>
+
+      {/* Main Content */}
+      <main className='relative w-full lg:max-w-7xl lg:mx-auto'>
+        <div className='flex flex-col lg:grid lg:grid-cols-2 lg:py-16'>
+          {/* Left content */}
+          <div className='my-auto'>
+            <div className='flex flex-col gap-6 lg:p-12 sm:p-4 text-center rounded-xl w-4/5 mx-auto my-8 text-white'>
+              <h1 className='text-5xl font-bold'>Welcome To Practice Pro</h1>
+              <p className='text-3xl'>Your Best Music Practice Diary</p>
+            </div>
           </div>
 
-          <div className='bg-white/90 w-4/5 h-[300px] mx-auto my-8 rounded-xl'>
-            Login/Signin form
+          {/* Right content (Login/Signup) */}
+          <div className='my-auto'>
+            <div className='bg-white/90 w-4/5 h-[600px] mx-auto rounded-xl'>
+              Login/Signin form
+            </div>
           </div>
-        </main>
-      </div>
+        </div>
+      </main>
     </div>
   );
-}
-
-{
-  /* <div className='flex flex-col gap-6 p-12 rounded-xl bg-black/90 w-4/5 sm:max-w-xl mx-auto text-white sm:text-2xl'>
-            <div className='flex flex-col gap-6 p-12 rounded-xl bg-black/90 w-4/5 sm:max-w-xl mx-auto text-white sm:text-2xl'>
-              <h1 className='text-4xl font-bold'>Welcome To Practice Pro</h1>
-              <p>Your Best Music Practice Diary</p>
-            </div>
-          </div>
-          <div>
-            <div className='flex flex-col justify-center gap-6 p-12 rounded-xl bg-white/90 w-3/5 h-3/5 mx-auto  sm:text-2xl'>
-              <h1 className='text-4xl font-bold'>Welcome To Practice Pro</h1>
-              <p>Your Best Music Practice Diary</p>
-            </div>
-          </div> */
 }
